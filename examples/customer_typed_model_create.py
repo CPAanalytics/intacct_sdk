921a61a59c54c17f3571753e03da9d485b0971f3
cpaanalytics <jmc083@gmail.com>
2026-01-13 11:10:07 -0600

    typed models

--- examples/customer_typed_model_create.py
+++ examples/customer_typed_model_create.py
@@ -8,17 +8,17 @@ def main() -> int:
     api = client.session_client(client.get_api_session())
 
     customer = CustomerModel(
-        customerid="CUST-001",
+        customerid="CUST-002",
         company='1002 - REPS',
         cust_cost_center='Saas',
-        name="Example Customer",
+        name="Example Customer 2",
         status="active",
         classes='202--SAAS-PRO',
         customer_peoaso='PRO',
 
     )
     result = api.objects.customer.create(customer)
-    print(result.fields)
+    print(result)
     return 0
 
 
